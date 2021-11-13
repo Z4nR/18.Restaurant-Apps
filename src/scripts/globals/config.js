5d3bbe76c3722938eb0aca5d050673925fc1386b
Zulham Ari <ari.comunity1@gmail.com>
2021-11-13 23:41:26 +0700

    add config and api-endpoint

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_IMG_URL: 'https://restaurant-api.dicoding.dev/images/medium/{pictureId}',
+  CACHE_NAME: new Date().toISOString()
+}
+
+export default CONFIG
